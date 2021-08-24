e6aa14278ca5a955f23a25ec66661c72468e0b24
Riya Kumari <56725036+Riya-code@users.noreply.github.com>
2021-08-24 12:51:28 +0530

    html template

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,HttpResponse
 from django.views import generic
 from .models import Post
 
+def fun(request):
+    return render(request,'pg.html')
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
